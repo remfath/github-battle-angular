e4a3a019ed77d6e96cde8114c4215bd1d3e7358b
remfath <remfath@gmail.com>
2017-06-11 21:18:28 +0800

    change css to scss and add more componet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BattleComponent } from './battle/battle.component';
+import { LoadingComponent } from './loading/loading.component';
+import { PopularComponent } from './popular/popular.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BattleComponent,
+    LoadingComponent,
+    PopularComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
