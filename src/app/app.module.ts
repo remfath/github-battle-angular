5cef200abeb446dcaec416c8570c46cfbc8028ca
remfath <remfath@gmail.com>
2017-06-11 23:34:15 +0800

    config route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { BattleComponent } from './battle/battle.component';
-import { LoadingComponent } from './loading/loading.component';
-import { PopularComponent } from './popular/popular.component';
-import { ResultComponent } from './result/result.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {BattleComponent} from './battle/battle.component';
+import {LoadingComponent} from './loading/loading.component';
+import {PopularComponent} from './popular/popular.component';
+import {ResultComponent} from './result/result.component';
+import {RouterModule} from '@angular/router';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
@@ -17,14 +19,30 @@ import { ResultComponent } from './result/result.component';
     BattleComponent,
     LoadingComponent,
     PopularComponent,
-    ResultComponent
+    ResultComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'battle',
+        component: BattleComponent
+      },
+      {
+        path: 'popular',
+        component: PopularComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
